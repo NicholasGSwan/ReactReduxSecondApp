c0664157c18cc49aa4fe9585b44f9d7610e93ca2
Nicholas Swan <nicholasgswan@gmail.com>
2018-04-20 12:07:43 -0500

    -promote book list from component to container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class Booklist extends Component{
+class BookList extends Component{
   renderList() {
     return this.props.books.map((book) => {
       return(
@@ -18,3 +19,13 @@ export default class Booklist extends Component{
   }
 
 }
+
+function mapStateToProps(state) {
+  //Whatever is returned will show up as props
+  // inside of BookList
+  return{
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
